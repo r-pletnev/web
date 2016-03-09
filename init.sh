66c5d845dc8c250d921d18add18a46eeac037f98
Roman <ge52@mail.ru>
2016-03-09 20:51:57 +0000

    static site is done!

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+rm /etc/nginx/sites-enabled/*.conf
+rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/guncorn.conf.py  /etc/gunicorn.d/test
-gunicorn -c /etc/gunicorn.d/test hello:app
+#sudo ln -s /home/box/web/etc/guncorn.conf.py  /etc/gunicorn.d/test
+gunicorn -c etc/gunicorn.conf.py hello:app
